63b8d241e6c9e1fdb76a38d64e18035a98c4e594
Ross Moug <ross.moug@n-able.com>
2023-05-19 15:24:52 +0100

    Setup federation

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import {loadRemoteModule} from '@angular-architects/native-federation';
 
 export const APP_ROUTES: Routes = [
     {
@@ -9,13 +10,13 @@ export const APP_ROUTES: Routes = [
       pathMatch: 'full'
     },
 
-
-    // Your route here:
-
-    // {
-    //   path: 'flights',
-    //   loadChildren: () => import('mfe1/Module').then(m => m.FlightsModule)
-    // },
+    {
+      path: 'flights',
+      loadChildren: () => loadRemoteModule({
+        remoteName: 'mfe1',
+        exposedModule: './Module'
+      }).then(m => m.FlightsModule)
+    },
 
     {
       path: '**',
